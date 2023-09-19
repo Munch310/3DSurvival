eefbadfccf9143589e2ae1c077845330638e92b9
Munch310 <ghkdlsi@naver.com>
2023-09-19 17:06:52 +0900

    [ADD] Day & Night
    - 낮, 밤 구현

---
+++ Assets/Scripts/Environments/DayNightCycle.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float time;
+    public float fullDayLength;
+    public float startTime = 0.4f;
+    private float timeRate;
+    public Vector3 noon;
+
+    [Header("Sun")]
+    public Light sun;
+    public Gradient sunColor;
+    public AnimationCurve sunIntensity;
+
+    [Header("Moon")]
+    public Light moon;
+    public Gradient moonColor;
+    public AnimationCurve moonIntensity;
+
+    [Header("Other Lighting")]
+    public AnimationCurve lightingIntensityMultiplier; // 환경광
+    public AnimationCurve reflectionIntensityMultiplier; // 반사광
+
+    private void Start()
+    {
+        timeRate = 1.0f / fullDayLength;
+        time = startTime;
+    }
+
+    private void Update()
+    {
+        time = (time + timeRate * Time.deltaTime) % 1.0f;
+        UpdateLighting(sun, sunColor, sunIntensity);
+        UpdateLighting(moon, moonColor, moonIntensity);
+
+        RenderSettings.ambientIntensity = lightingIntensityMultiplier.Evaluate(time);
+        RenderSettings.reflectionIntensity = reflectionIntensityMultiplier.Evaluate(time);
+    }
+
+    void UpdateLighting(Light lightSource, Gradient colorGradiant, AnimationCurve intensityCurve)
+    {
+        float intensity = intensityCurve.Evaluate(time);
+
+        lightSource.transform.eulerAngles = (time - (lightSource == sun ? 0.25f : 0.75f)) * noon * 4.0f; // sun일떄는 0.25(1/4지점) moon일 떄는 0.75 * 90도
+        lightSource.color = colorGradiant.Evaluate(time);
+        lightSource.intensity = intensity;
+
+        GameObject go = lightSource.gameObject;
+        if (lightSource.intensity == 0 && go.activeInHierarchy)
+            go.SetActive(false);
+        else if (lightSource.intensity > 0 && !go.activeInHierarchy)
+            go.SetActive(true);
+    }
+}
